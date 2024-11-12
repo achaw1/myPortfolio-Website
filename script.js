b99e697a49c97edd03713f4493c82d79fd8c7e8d
achaw1 <michael.owusu_achaw@lethbridgecollege.ca>
2024-11-11 19:59:24 -0700

    15% done

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.queryselector(".menu-links")
+    const icon = document.queryselector(".hamburger-icon")
+    menu.classList.toggle("menu")
+    icon.classList.toggle("menu")
+}
